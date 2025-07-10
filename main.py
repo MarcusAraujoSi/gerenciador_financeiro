ac9231055b71839ccbe7ee77439eb39614704532
MarcusAraujoSi <si.marcusaraujo@gmail.com>
2025-07-10 00:55:37 -0300

    Adiciona suporte a CORS no back-end para integração com front

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.routes import transactions_router, category_router, summary_router
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize the FastAPI application
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://192.168.68.106:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the transaction and category routers under the /api prefix
 app.include_router(transactions_router.router, prefix="/api")
